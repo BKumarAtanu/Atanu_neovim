c47e2331d20b01bf222b53758deb85adac181b6d
BKumarAtanu <49538916+BKumarAtanu@users.noreply.github.com>
2024-06-07 14:49:18 +0600

    init.lua file created, README.md file created, nvim.svg file added for vim logo

---
+++ init.lua
@@ -0,0 +1,4 @@
+ 
+-- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
+-- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
+
